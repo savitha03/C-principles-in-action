e357f74c6d616a35cce467c63f393be1b834485c
savitha03 <savithaworks0203@gmail.com>
2024-11-13 12:14:31 +0530

    Add files via upload

---
+++ ANIMAL/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+    abstract class Animal
+    {
+        public string Name { get; private set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+       
+        public abstract void Speak();
+    }
+
+    class Dog : Animal
+    {
+        public Dog(string name) : base(name) { }
+
+      
+        public override void Speak()
+        {
+            Console.WriteLine($"{Name} says: Woof!");
+        }
+    }
+
+   
+    class Cat : Animal
+    {
+        public Cat(string name) : base(name) { }
+
+        
+        public override void Speak()
+        {
+            Console.WriteLine($"{Name} says: Meow!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Animal myDog = new Dog("Buddy");
+            Animal myCat = new Cat("Whiskers");
+
+            myDog.Speak();  
+            myCat.Speak();  
+        }
+    }
